25f926e3272ef8d2b366e2ad639144681bf2524d
MillicentMal <millicentmalinga19@gmail.com>
2021-07-23 06:47:59 +0000

    Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,11 +11,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
-if (!parent) 
-{
 parent = (binary_tree_t *)malloc(sizeof(parent));
 parent->value = value;
-}
 parent->left = parent->right = NULL;
 return (parent);
 }
