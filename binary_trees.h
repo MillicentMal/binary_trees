c44ce8e82d70a733513ee23fedececbb621f9a92
MillicentMal <millicentmalinga19@gmail.com>
2021-07-23 07:47:31 +0000

    Task 0

--- binary_trees.h
+++ binary_trees.h
@@ -2,13 +2,13 @@
 #define BINARY_TREES_H
 
 /**
- *  * struct binary_tree_s - Binary tree node
- *   *
- *    * @n: Integer stored in the node
- *     * @parent: Pointer to the parent node
- *      * @left: Pointer to the left child node
- *       * @right: Pointer to the right child node
- *        */
+ * struct binary_tree_s - Binary tree node
+ *
+ *@n: Integer stored in the node
+ *@parent: Pointer to the parent node
+ *@left: Pointer to the left child node
+ *@right: Pointer to the right child node
+ **/
 struct binary_tree_s
 {
 	    int n;
