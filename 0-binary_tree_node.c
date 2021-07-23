09d0dbe6c34b8f907b97b5b992c1b3e0f13b57a5
MillicentMal <millicentmalinga19@gmail.com>
2021-07-23 08:20:39 +0000

    Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,18 +10,18 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *rootptr;
-rootptr = malloc(sizeof(rootptr));
-if (!rootptr)
+binary_tree_t *new_node;
+new_node = (new_node *)malloc(sizeof(new_node));
+if (!new_node)
 {
 return (NULL);
 }
-rootptr->n = value;
-rootptr->parent = parent;
-rootptr->left =  NULL;
-rootptr->right = NULL;
-free(rootptr);
-return (rootptr);
+new_node->n = value;
+new_node->parent = parent;
+new_node->left =  NULL;
+new_node->right = NULL;
+free(new_node);
+return (new_node);
 
 }
 
